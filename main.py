1ad186a282cd361445a513a19b38dab49792795e
lixiaojing001 <73015014+lixiaojing001@users.noreply.github.com>
2021-08-26 12:42:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ step1 = ""
 # 开启根据地区天气情况降低步数（默认关闭）
 open_get_weather = sys.argv[12]
 # 设置获取天气的地区（上面开启后必填）如：area = "宁波"
-area = sys.argv[13]
+
 
 # 以下如果看不懂直接默认就行只需改上面
 
@@ -52,37 +52,6 @@ now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 headers = {'User-Agent': 'MiFit/5.3.0 (iPhone; iOS 14.7.1; Scale/3.00)'}
 
 
-#获取区域天气情况
-def getWeather():
-    if area == "NO":
-        return
-    else:
-        global K, type
-        url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + area
-        hea = {'User-Agent': 'Mozilla/5.0'}
-        r = requests.get(url=url, headers=hea)
-        if r.status_code == 200:
-            result = r.text
-            res = json.loads(result)
-            if "多云" in res['data']['forecast'][0]['type']:
-                K = K_dict["多云"]
-            elif "阴" in res['data']['forecast'][0]['type']:
-                K = K_dict["阴"]
-            elif "小雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["小雨"]
-            elif "中雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["中雨"]
-            elif "大雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["大雨"]
-            elif "暴雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["暴雨"]
-            elif "大暴雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["大暴雨"]
-            elif "特大暴雨" in res['data']['forecast'][0]['type']:
-                K = K_dict["特大暴雨"]
-            type = res['data']['forecast'][0]['type']
-        else:
-            print("获取天气情况出错")
 
 
 #获取北京时间确定随机步数&启动主函数
